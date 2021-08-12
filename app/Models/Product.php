dcda2af2440bef7a4e4f616ad82c2c417c496035
Ranieli Silveira <ranielisilveira@hotmail.com>
2021-08-11 21:32:23 -0300

    feat: products models and migrations

---
+++ app/Models/Product.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'sku_code',
+        'category_id',
+        'name',
+        'price',
+        'composition',
+        'size',
+        'stock',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class, 'product_id');
+    }
+}
---
+++ database/migrations/2021_08_12_000702_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('sku_code')->unique();
+            $table->bigInteger('category_id')->unsigned();
+            $table->string('name');
+            $table->decimal('price', 12, 2)->default(0);
+            $table->text('composition');
+            $table->string('size')->nullable();
+            $table->integer('stock')->unsigned()->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
