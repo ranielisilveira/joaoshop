d625da608ad3c89118b5c8d827d4b3adfea02d03
Ranieli Silveira <ranielisilveira@hotmail.com>
2021-08-11 21:04:40 -0300

    feat: sanctum auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ], [
+            'name.required' => 'Você deve fornecer seu nome.',
+            'name.max' => 'O nome fornecido é muito extenso.',
+            'email.required' => 'Você deve fornecer um email.',
+            'email.email' => 'Você deve fornecer um email válido.',
+            'email.unique' => 'O email informado é inválido.',
+            'password.required' => 'Uma senha deve ser informada.',
+            'password.min' => 'A senha deve conter ao menos 8 dígitos.',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Credenciais inválidas.'
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $user = User::where('email', $request->email)->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'message' => 'Você deslogou com sucesso'
+        ]);
+    }
+}
